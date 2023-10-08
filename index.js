e75e8be424e119f133375f336cdc5f837e6403d6
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-08 15:26:13 +0300

    initial, to add pseudocode

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j=i+1; j < array.length; j++) {
+      sum = array[i] + array[j];
+      if (sum === target) {
+        return true;
+      }
+    }
+  }return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
